d305ccc5ce466c38b7833f9f64b4e497ba382211
Aziret <azi326064@gmail.com>
2024-09-12 16:18:01 +0300

    update-UI-button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -22,17 +22,30 @@ export const Button = ({
 };
 
 const StyledButton = styled(MuiButton)(({ variant }) => ({
+  width: "100px",
+  height: "40px",
+  borderRadius: "0.625rem",
+
+  background:
+    "linear-gradient(to bottom,rgba(12, 187, 107, 1),rgba(2, 123, 68, 1)) 100%",
+  color: "white",
+
+  "&:hover": {
+    background: "linear-gradient(rgba(8, 223, 125, 1), rgba(4, 143, 80, 1))",
+  },
   ...(variant === "contained" && {
     width: "22.75rem",
     height: "2.625rem",
     borderRadius: "0.625rem",
+
     padding: "14px, 32px, 14px, 32px",
     background:
       "linear-gradient(to bottom,rgba(12, 187, 107, 1),rgba(2, 123, 68, 1)) 100%",
-    borderBlock: "1px black",
+
     "&:hover": {
       background: "linear-gradient(rgba(8, 223, 125, 1), rgba(4, 143, 80, 1))",
     },
+
     "&:disabled": {
       background: "rgba(211, 211, 211, 1)",
       width: "22.75rem",
@@ -44,27 +57,22 @@ const StyledButton = styled(MuiButton)(({ variant }) => ({
   ...(variant === "outlined" && {
     width: "6.938rem",
     height: "2.625rem",
+
     borderColor: "rgba(4, 135, 65, 1)",
     borderRadius: "0.625rem",
     color: "rgba(4, 135, 65, 1)",
+    background: "transparent",
+
     padding: "10px, 20px, 10px, 20px",
     "&:hover": {
       background:
         "linear-gradient(to bottom,rgba(12, 187, 107, 1),rgba(2, 123, 68, 1)) 100%",
       color: "white",
+      border: "none",
     },
     "&:disabled": {
       width: "6.938rem",
       height: "2.625rem",
     },
   }),
-  width: "100px",
-  height: "40px",
-  borderRadius: "0.625rem",
-  background:
-    "linear-gradient(to bottom,rgba(12, 187, 107, 1),rgba(2, 123, 68, 1)) 100%",
-  color: "white",
-  "&:hover": {
-    background: "linear-gradient(rgba(8, 223, 125, 1), rgba(4, 143, 80, 1))",
-  },
 }));
