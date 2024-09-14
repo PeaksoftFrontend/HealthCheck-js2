6559686953ff49c62452896007f90f0e0bf7955b
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2024-09-14 19:10:00 +0300

    finished-icons-logo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export const config = defineConfig({
+export default defineConfig({
   plugins: [react(), svgr()],
 });
