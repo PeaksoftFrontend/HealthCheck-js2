c6d3bee2872b63c782de267120d3d1068f549bed
Nooruz <abdykalykovnooruz638@gmail.com>
2024-09-17 02:50:41 +0300

    clear app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { styled } from "@mui/material";
 export const App = () => {
   return (
-    <StyledDiv>
+    <div>
       <h1>Healtcheck</h1>
-    </StyledDiv>
+    </div>
   );
 };
-
-const StyledDiv = styled("div")(({ theme }) => ({
-  background: theme.palette.primary.gradientPrimary,
-}));
