bbf153388c348985de37265fbe64220532f2564a
Aziret <azi326064@gmail.com>
2024-09-19 15:08:33 +0300

    addForwardRef/toSelect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { Select } from "./components/UI/Select";
-
 export const App = () => {
-  const name = [
-    {
-      label: "aziret",
-    },
-    {
-      label: "aza",
-    },
-    {
-      label: "azi",
-    },
-  ];
   return (
     <div>
       <h1>Healthcheck</h1>
-      <Select options={name} placeholder={"kfajdshf"} label={"akfjdhsfha"} />
     </div>
   );
 };
