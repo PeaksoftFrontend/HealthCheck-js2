f482230010ae1178bab26ae80c462f3862fcb21c
AidanaAbdurazakovna <abdurazakova.aidana@gmail.com>
2024-09-19 13:34:24 +0300

    snackbar and font finished

---
+++ src/assets/images/icons/index.js
@@ -0,0 +1,139 @@
+import LogoHalthCeck from "./logo/logo-healthCheck.svg?react";
+
+import Watsapp from "./messenger/whatsApp.svg?react";
+import Telegram from "./messenger/telegram.svg?react";
+import Instagram from "./messenger/instagram.svg?react";
+import GreenWatsapp from "./messenger/green-watsApp.svg?react";
+import GreenTelegram from "./messenger/green-telegram.svg?react";
+import GreenInstagram from "./messenger/green-instagram.svg?react";
+
+import AllIcons from "./others/allicons.svg?react";
+import andDesginPhoneOutlined from "./others/ant-design_phone-outlined.svg?react";
+import Bicons from "./others/B.svg?react";
+import burgerMenu from "./others/burger-menu.svg?react";
+import Calander from "./others/calendar.svg?react";
+import CalendarTodayIcon from "./others/CalendarTodayIcon.svg?react";
+import Cancellation from "./others/cancellation.svg?react";
+import Cansled from "./others/canseled.svg?react";
+import CheckboxFill from "./others/checkbox-fill.svg?react";
+import CheckboxRound from "./others/checkbox-round.svg?react";
+import DeleteOutlineclcon from "./others/DeleteOutlineIcon.svg?react";
+import Down from "./others/down.svg?react";
+import Frame from "./others/Frame.svg?react";
+import Gps from "./others/gps.svg?react";
+import Iicons from "./others/I.svg?react";
+import Img from "./others/img.svg?react";
+import Loading from "./others/loading.svg?react";
+import MenuLi from "./others/menu-li.svg?react";
+import MenuOl from "./others/menu-ol.svg?react";
+import Message from "./others/message-icons.svg?react";
+import ModeEdition from "./others/ModeEditIcon.svg?react";
+import Phone from "./others/phone.svg?react";
+import Plus from "./others/plus.svg?react";
+import Print from "./others/print 1.svg?react";
+import Profile from "./others/Profile.svg?react";
+import Recorded from "./others/recorded.svg?react";
+import Reverse from "./others/reverse.svg?react";
+import Search from "./others/search.svg?react";
+import Up from "./others/up.svg?react";
+import Users2 from "./others/Users-2.svg?react";
+import Users from "./others/users.svg?react";
+import VisibilituIcon from "./others/VisibilityIcon.svg?react";
+import VisibilityOffIcon from "./others/VisibilityOffIcon.svg?react";
+
+import Check1 from "./toggle/check-1.svg?react";
+import CheckboxEmpty from "./toggle/checkbox-empty.svg?react";
+import Checkbox from "./toggle/checkbox.svg?react";
+import Swich from "./toggle/Switch.svg?react";
+
+import Analysic from "./service-icons/analysis.svg?react";
+import Brain from "./service-icons/brain.svg?react";
+import Easy from "./service-icons/easy.svg?react";
+import Endokrinologiya from "./service-icons/Endokrinologiya.svg?react";
+import Eye from "./service-icons/eye.svg?react";
+import Fizioterapiya from "./service-icons/Fizioterapiya.svg?react";
+import Gynecology from "./service-icons/gynecology.svg?react";
+import HairRoots from "./service-icons/hair-roots.svg?react";
+import Hand from "./service-icons/hand.svg?react";
+import Heart from "./service-icons/heart.svg?react";
+import Intestines from "./service-icons/intestines.svg?react";
+import Legs from "./service-icons/legs.svg?react";
+import Neurosurgery from "./service-icons/neurosurgery.svg?react";
+import Nose from "./service-icons/nose.svg?react";
+import Onkologiya from "./service-icons/Onkologiya.svg?react";
+import Ortopediya from "./service-icons/Ortopediya.svg?react";
+import Oxygen from "./service-icons/oxygen.svg?react";
+import Psikhoterapiya from "./service-icons/Psikhoterapiya.svg?react";
+import Revmatologiya from "./service-icons/Revmatologiya.svg?react";
+import Stomach from "./service-icons/stomach.svg?react";
+import Terapiya from "./service-icons/Terapiya.svg?react";
+import Urologiya from "./service-icons/Urologiya.svg?react";
+
+export const Icons = {
+  LogoHalthCeck,
+  Watsapp,
+  Telegram,
+  Instagram,
+  GreenWatsapp,
+  GreenInstagram,
+  GreenTelegram,
+  AllIcons,
+  andDesginPhoneOutlined,
+  Bicons,
+  burgerMenu,
+  Calander,
+  CalendarTodayIcon,
+  Cancellation,
+  Cansled,
+  CheckboxFill,
+  CheckboxRound,
+  DeleteOutlineclcon,
+  Down,
+  Frame,
+  Gps,
+  Iicons,
+  Img,
+  Loading,
+  MenuLi,
+  MenuOl,
+  Message,
+  ModeEdition,
+  Phone,
+  Plus,
+  Print,
+  Profile,
+  Recorded,
+  Reverse,
+  Search,
+  Up,
+  Users2,
+  Users,
+  VisibilituIcon,
+  VisibilityOffIcon,
+  Check1,
+  CheckboxEmpty,
+  Checkbox,
+  Swich,
+  Analysic,
+  Brain,
+  Easy,
+  Endokrinologiya,
+  Eye,
+  Fizioterapiya,
+  Gynecology,
+  HairRoots,
+  Hand,
+  Heart,
+  Intestines,
+  Legs,
+  Neurosurgery,
+  Nose,
+  Onkologiya,
+  Ortopediya,
+  Oxygen,
+  Psikhoterapiya,
+  Revmatologiya,
+  Stomach,
+  Terapiya,
+  Urologiya,
+};
