596b6fd37a0b270de240e1a12645b001bcfeaebe
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2024-09-22 21:06:48 +0300

    fifished-slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,26 +1,15 @@
 import { styled } from "@mui/material";
 import { SliderItem } from "./SliderItem";
-import { useEffect, useRef, useState } from "react";
+import { useState, useRef } from "react";
 import { reviews } from "../../utils/constants/constants";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export const SliderComponent = () => {
+export const SliderReview = () => {
   const [activeStep, setActiveStep] = useState(0);
   const sliderRef = useRef(null);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (sliderRef.current) {
-        const nextStep = (activeStep + 1) % reviews.length;
-        sliderRef.current.slickGoTo(nextStep);
-      }
-    }, 4000);
-
-    return () => clearInterval(interval);
-  }, [activeStep]);
 
   const settings = {
     dots: true,
@@ -31,11 +20,9 @@ export const SliderComponent = () => {
     slidesToScroll: 1,
     speed: 800,
     arrows: true,
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-    beforeChange: (current, next) => {
-      setActiveStep(next);
-    },
+    prevArrow: <Arrow direction="left" />,
+    nextArrow: <Arrow direction="right" />,
+    beforeChange: (_, next) => setActiveStep(next),
     customPaging: (i) => <CustomDot isActive={i === activeStep} />,
   };
 
@@ -66,7 +53,7 @@ const CustomDot = styled("div")(({ isActive }) => ({
   transition: "background-color 0.3s ease",
 }));
 
-const StyledContainer = styled("div")(() => ({
+const StyledContainer = styled("div")({
   width: "100%",
   height: "30.0625rem",
   display: "flex",
@@ -81,20 +68,15 @@ const StyledContainer = styled("div")(() => ({
   "& span": {
     color: "#28a745",
   },
-}));
+});
 
-const PrevArrow = ({ onClick }) => (
-  <ArrowButton onClick={onClick} left>
-    {"<"}
-  </ArrowButton>
-);
-const NextArrow = ({ onClick }) => (
-  <ArrowButton onClick={onClick} right>
-    {">"}
+const Arrow = ({ direction, onClick }) => (
+  <ArrowButton onClick={onClick} direction={direction}>
+    {direction === "left" ? "<" : ">"}
   </ArrowButton>
 );
 
-const ArrowButton = styled("div")(({ left, right }) => ({
+const ArrowButton = styled("div")(({ direction }) => ({
   position: "absolute",
   top: "95%",
   zIndex: 1,
@@ -108,11 +90,10 @@ const ArrowButton = styled("div")(({ left, right }) => ({
   justifyContent: "center",
   alignItems: "center",
   cursor: "pointer",
-  fontWeight: "900",
-  transform: "translateX(0%)",
+  fontWeight: "700",
+  left: direction === "left" ? "41%" : "auto",
+  right: direction === "right" ? "41%" : "auto",
   transition: "all 0.3s ease",
-  ...(left && { left: "41%" }),
-  ...(right && { right: "41%" }),
   "&:hover": {
     backgroundColor: "#0CBB6B",
     color: "#fff",
