f19abb447733a4f9e0b99f46bf17ab8fc97feddb
AidanaAbdurazakovna <abdurazakova.aidana@gmail.com>
2024-09-23 22:33:36 +0300

    separated the components

--- src/components/services/MainService.jsx
+++ src/components/services/MainService.jsx
@@ -1,27 +1,5 @@
-import "@fontsource/manrope";
-
-import icon1 from "../../assets/icons/service-icons/Gematologicheskoe_testirovanie.png";
-import icon2 from "../../assets/icons/service-icons/heart.svg";
-import icon3 from "../../assets/icons/service-icons/brain.svg";
-import icon4 from "../../assets/icons/service-icons/hair-roots.svg";
-import icon5 from "../../assets/icons/service-icons/eye.svg";
-import icon6 from "../../assets/icons/service-icons/Terapiya.svg";
-import icon7 from "../../assets/icons//service-icons/Fizioterapiya.svg";
-import icon8 from "../../assets/icons/service-icons/Onkologiya.svg";
-import icon9 from "../../assets/icons/service-icons/Urologiya.svg";
 import { styled } from "@mui/material";
-
-const services = [
-  { icon: icon1, text: "Вакцинация" },
-  { icon: icon2, text: "Кардиология" },
-  { icon: icon3, text: "Неврология" },
-  { icon: icon4, text: "Дерматология" },
-  { icon: icon5, text: "Офтальмология" },
-  { icon: icon6, text: "Терапия" },
-  { icon: icon7, text: "Физиотерапия" },
-  { icon: icon8, text: "Онкология" },
-  { icon: icon9, text: "Урология" },
-];
+import { services } from "../../utils/constants/service";
 
 export const MainServices = () => {
   return (
---
+++ src/utils/constants/service.js
@@ -0,0 +1,21 @@
+import icon1 from "../../assets/icons/service-icons/Gematologicheskoe_testirovanie.png";
+import icon2 from "../../assets/icons/service-icons/heart.svg";
+import icon3 from "../../assets/icons/service-icons/brain.svg";
+import icon4 from "../../assets/icons/service-icons/hair-roots.svg";
+import icon5 from "../../assets/icons/service-icons/eye.svg";
+import icon6 from "../../assets/icons/service-icons/Terapiya.svg";
+import icon7 from "../../assets/icons//service-icons/Fizioterapiya.svg";
+import icon8 from "../../assets/icons/service-icons/Onkologiya.svg";
+import icon9 from "../../assets/icons/service-icons/Urologiya.svg";
+
+export const services = [
+  { icon: icon1, text: "Вакцинация" },
+  { icon: icon2, text: "Кардиология" },
+  { icon: icon3, text: "Неврология" },
+  { icon: icon4, text: "Дерматология" },
+  { icon: icon5, text: "Офтальмология" },
+  { icon: icon6, text: "Терапия" },
+  { icon: icon7, text: "Физиотерапия" },
+  { icon: icon8, text: "Онкология" },
+  { icon: icon9, text: "Урология" },
+];
