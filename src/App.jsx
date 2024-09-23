70c68d56bfee96afab6e224e43e50d879d44d8bd
AidanaAbdurazakovna <abdurazakova.aidana@gmail.com>
2024-09-23 18:28:33 +0300

    ready mainService

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { MainServices } from "./mainservices/MainServices";
-
 export const App = () => {
   return (
     <div>
-      {/* <h1>Healthcheck</h1> */}
-      <MainServices />
+      <h1>Healthcheck</h1>
     </div>
   );
 };
