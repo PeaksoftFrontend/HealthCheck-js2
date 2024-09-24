37ebd6210b3531da368358dd4d674775e5494546
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2024-09-24 21:55:48 +0300

    chenged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { MainLanding } from "./page/landing/MainLanding";
-
 export const App = () => {
   return (
     <div>
       <h1>Healtcheck</h1>
-      <MainLanding />
     </div>
   );
 };
