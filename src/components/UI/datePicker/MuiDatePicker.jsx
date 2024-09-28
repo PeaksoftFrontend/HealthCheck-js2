c36ba0fecc8e9466cfd152e8f059a0cc0687a10e
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2024-09-28 03:39:58 +0300

    ui-datePicker

--- src/components/UI/datePicker/MuiDatePicker.jsx
+++ src/components/UI/datePicker/MuiDatePicker.jsx
@@ -1,39 +1,39 @@
-import { styled, TextField, InputAdornment } from "@mui/material";
+import { styled } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useState } from "react";
 import dayjs from "dayjs";
+import "dayjs/locale/ru";
+import { Icons } from "../../../assets/icons";
+import { TextField, InputAdornment } from "@mui/material";
 
-export const MuiDatePicker = ({ placeholder, labelText }) => {
-  const [selectedDate, setSelectedDate] = useState(null);
-
+export const MuiDatePicker = ({ selectedDate, onDateChange }) => {
   const shouldDisableDate = (date) => {
     return date.isBefore(dayjs(), "day");
   };
 
   return (
-    <div>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <StyledDatePicker
-          value={selectedDate}
-          onChange={(newValue) => setSelectedDate(newValue)}
-          shouldDisableDate={shouldDisableDate}
-          inputFormat="DD.MM.YY"
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              placeholder={placeholder}
-              InputProps={{
-                startAdornment: labelText && (
-                  <InputAdornment position="start">{labelText}</InputAdornment>
-                ),
-              }}
-            />
-          )}
-        />
-      </LocalizationProvider>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+      <StyledDatePicker
+        value={selectedDate}
+        onChange={(newValue) => onDateChange(newValue)}
+        shouldDisableDate={shouldDisableDate}
+        inputFormat="DD.MM.YY"
+        renderInput={(params) => (
+          <StyledTextField
+            {...params}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Icons.CalendarTodayIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        )}
+      />
+    </LocalizationProvider>
   );
 };
 
@@ -48,12 +48,18 @@ const StyledDatePicker = styled(DatePicker)(() => ({
       WebkitTransition: "color 9999s ease-out, background-color 9999s ease-out",
       WebkitTransitionDelay: "9999s",
     },
+
   "& .MuiInputBase-input": {
     border: "none",
-    padding: "8px 2px",
+    padding: "8px 4px",
   },
-  "& fieldset": { border: "none" },
+
+  "& fieldset": {
+    border: "none",
+  },
   "& ::-webkit-input-placeholder": {
     color: "#959595",
   },
 }));
+
+const StyledTextField = styled(TextField)(() => ({}));
