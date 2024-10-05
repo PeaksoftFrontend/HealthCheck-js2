1e4e187af110849f070f773909cd8eba17a41cf8
AidanaAbdurazakovna <abdurazakova.aidana@gmail.com>
2024-10-05 22:22:34 +0300

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AdminHeader } from "./components/adminHeader/AdminHeader";
 export const App = () => {
   return (
     <div>
-      <h1> Healthcheck</h1>
+      {/* <h1> Healthcheck</h1> */}
       <AdminHeader />
     </div>
   );
--- src/components/adminHeader/AdminHeader.jsx
+++ src/components/adminHeader/AdminHeader.jsx
@@ -1,14 +1,12 @@
-import { styled } from "@mui/material";
+import { styled, MenuItem, Select, FormControl } from "@mui/material";
 import { Icons } from "../../assets/icons";
 
 export const AdminHeader = () => {
   return (
     <StyledHeader>
-      <nav>
-        <StyledNavContainer>
-          <StyledLogoContainer>
-            <Icons.LogohealthCheck />
-          </StyledLogoContainer>
+      <StyledNavContainer>
+        <StyledContainerNav>
+          <Icons.LogohealthCheck />
           <StyledNavLinks>
             <a href="#" className="active">
               Онлайн-запись
@@ -17,82 +15,78 @@ export const AdminHeader = () => {
             <a href="#">Специалисты</a>
             <a href="#">Пациенты</a>
           </StyledNavLinks>
-          <StyledSelect>
-            <select>
-              <option>Администратор </option>
-              <option>Профиль</option>
-              <option>Войти</option>
-            </select>
-          </StyledSelect>
-        </StyledNavContainer>
-      </nav>
+        </StyledContainerNav>
+        <FormControlStyled>
+          <Select defaultValue="Администратор">
+            <MenuItem value="Администратор">Администратор</MenuItem>
+            <MenuItem value="Профиль">Профиль</MenuItem>
+            <MenuItem value="Выйти">Выйти</MenuItem>
+          </Select>
+        </FormControlStyled>
+      </StyledNavContainer>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled("header")(() => ({
-  display: "contents",
-  justifyContent: "normal",
-  boxShadow: "initial",
   width: "100%",
-  height: "5rem",
-  padding: "5rem",
-  alignItems: "center",
-  backgroundColor: "#ffffff",
-}));
-
-const StyledNavContainer = styled("div")(() => ({
   display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  width: "100%",
-  padding: "3rem",
-}));
-
-const StyledLogoContainer = styled("div")(() => ({
-  width: "14rem",
-  height: "4rem",
-  display: "flex",
-  alignItems: "flex-start",
   justifyContent: "center",
-  marginTop: "-1rem",
+  boxShadow: "0 0 5px rgb(0 0 0 /0.1)",
+  paddingBottom: "8px",
+}));
+
+const StyledNavContainer = styled("nav")(() => ({
+  maxWidth: "1450px",
+  height: "80px",
+  display: "flex",
+  gap: "227px",
 }));
 
 const StyledNavLinks = styled("div")(() => ({
   display: "flex",
-  gap: "3rem",
-  fontSize: "1rem",
+  gap: "30px",
   fontWeight: "500",
   color: "#333",
-  alignItems: "flex-end",
   justifyContent: "center",
+  alignItems: "center",
+
   "& a": {
     textDecoration: "none",
     color: "#333",
-    paddingBottom: "0.5rem",
-    borderBottom: "2px solid transparent",
-    "&:hover": {
-      color: "#0b0c0b",
-      borderBottom: "2px solid #299b51",
-      fontWeight: "bold",
-    },
   },
 }));
 
-const StyledSelect = styled("div")(() => ({
-  padding: "2rem",
-  position: "relative",
-  "& select": {
-    padding: "0.5rem 1rem",
+const FormControlStyled = styled(FormControl)(() => ({
+  minWidth: "160px",
+  padding: "1rem",
+
+  "& .MuiInputBase-root": {
+    padding: " 1rem",
     fontSize: "1rem",
-    backgroundColor: "#fff",
     borderRadius: "0.375rem",
-    border: "none",
+    backgroundColor: "#fff",
     color: "#090808",
-    cursor: "pointer",
-    transition: "border-color 0.3s",
-    "&:focus": {
-      outline: "none",
-    },
+    border: "none",
+    boxShadow: "none",
   },
+
+  "& .MuiOutlinedInput-notchedOutline": {
+    border: "none",
+  },
+
+  "& .MuiSelect-root:hover": {
+    backgroundColor: "#fff",
+  },
+
+  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+    border: "none",
+  },
+}));
+
+const StyledContainerNav = styled("div")(() => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "160px",
+  paddingTop: "16px",
 }));
