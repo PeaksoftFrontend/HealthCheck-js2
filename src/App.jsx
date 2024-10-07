cb7d3febd0044a320784f4f5fa20d7746e3ff15d
Aziret <azi326064@gmail.com>
2024-10-07 15:27:47 +0300

    add signin signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import { Modal } from "./components/UI/modal/Modal";
+import { Button } from "./components/UI/button/Button";
+// import { ForgotPassword } from "./components/auth/ForgotPassword";
+// import { ChangePassword } from "./components/auth/ChangePassword";
+// import { SignIn } from "./components/auth/SignIn";
+// import { SignUp } from "./components/auth/SignUp";
+// import { ForgotPassword } from "./components/auth/ForgotPassword";
+import { SignIn } from "./components/auth/SignIn";
+// import { SignIn } from "./components/auth/SignIn";
+
 export const App = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
   return (
     <div>
-      <h1> Healthcheck</h1>
+      <h1>Healtcheck</h1>
+      <Modal isOpen={isOpenModal} onClose={handleCloseModal}>
+        <SignIn />
+      </Modal>
+      <Button onClick={() => handleOpenModal()}>SignIn</Button>
     </div>
   );
 };
