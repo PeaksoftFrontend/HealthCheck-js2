1d003df1b9667107a2c0a0bf4b7737128f0cb889
zhakshylyk <zhakshylyk@macs-MacBook-Pro.local>
2024-10-10 15:48:35 +0300

    resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { MainLanding } from "./page/landing/MainLanding";
-
 export const App = () => {
   return (
     <div>
-      {/* <h1> Healthcheck</h1> */}
-      <MainLanding />
+      <h1> Healthcheck</h1>
     </div>
   );
 };
