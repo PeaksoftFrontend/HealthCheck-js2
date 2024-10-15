7de01b409038c4ed86cb2395abcc14d6ea7678fb
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2024-10-15 17:09:52 +0300

    fixed-errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { AddSpecialist } from "./components/admin/AddSpecialist";
-
 export const App = () => {
   return (
     <div>
       <h1> Healthcheck</h1>
-      <AddSpecialist />
     </div>
   );
 };
