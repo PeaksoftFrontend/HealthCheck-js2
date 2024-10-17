2a76f5bd40e83a2f3abf275a5ad4fd450ed81fdc
Aziret <azi326064@gmail.com>
2024-10-18 00:32:04 +0300

    finished/aboutTheClinic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { AboutTheClinic } from "./page/aboutClinic/AboutTheClinic";
-
 export const App = () => {
   return (
     <div>
       <h1>Healtcheck</h1>
-      <AboutTheClinic />
     </div>
   );
 };
