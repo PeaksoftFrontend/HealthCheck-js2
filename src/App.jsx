2ab80304f3c0731bce51cfa24618d0dd51188ec4
AidanaAbdurazakovna <abdurazakova.aidana@gmail.com>
2024-10-18 17:48:04 +0300

    finished Adding Doctors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { AddingDoctors } from "./components/addingDoctors/AddingDoctors";
+
 export const App = () => {
   return (
     <div>
-      <h1> Healthcheck</h1>{" "}
+      {/* <h1> Healthcheck</h1> */}
+      <AddingDoctors />
     </div>
   );
 };
