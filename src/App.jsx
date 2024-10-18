0a37945da43b27c051b8d74efa181bfaba1e5640
AidanaAbdurazakovna <abdurazakova.aidana@gmail.com>
2024-10-18 17:56:11 +0300

    resolved conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { AddingDoctors } from "./components/addingDoctors/AddingDoctors";
-
 export const App = () => {
   return (
     <div>
-      {/* <h1> Healthcheck</h1> */}
-      <AddingDoctors />
+      <h1> Healthcheck</h1>
     </div>
   );
 };
